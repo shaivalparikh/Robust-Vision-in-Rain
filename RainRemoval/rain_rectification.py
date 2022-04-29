dab0e4e3de78d23826383ba57a0e727e6fd0230e
Gregory Su <gtsu@andrew.cmu.edu>
2022-04-28 20:27:40 -0400

    Fix RR Individual Demo Code

--- RainRemoval/rain_rectification.py
+++ RainRemoval/rain_rectification.py
@@ -7,26 +7,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 import time
+import sys
 sys.path.append('../RainRemoval/')
 import RainRemoval
 
 sys.path.append('../Detectron2Predictor/')
 import detectron2_predictor as d2
 
-path = ""
-output_path = ""
-detectron = Detectron2Predictor('SemanticSegmentation', model_path='../clear_model_final.pth')
+path = "../DrivingScripts/carla_data"
+output_path = path
+detectron = d2.Detectron2Predictor('SemanticSegmentation', model_path='../clear_model_final.pth')
 remover = RainRemoval.RainRemoval('../40000_carla.pth')
-start = time.time()
+count = 0
+total = 0
 for file in glob.glob(path+"/*.png"):
     name = file.split("/")[-1]
+   
     if "_rain_" in file:
+        count += 1
+        print(name)
         array = Image.open(file)
         array = np.asarray(array)
+        start = time.time()
         report = remover.infer(array)
         array_rr = report[0]
         output = detectron.test_image(array_rr)
-
+        end = time.time()
+        total += end - start
         # fig = plt.figure(figsize=(14, 7))
         
         # fig.add_subplot(1,3,1)
@@ -43,10 +50,11 @@ for file in glob.glob(path+"/*.png"):
         
         # plt.savefig(output_path+"/"+name)
 
-        op = np.hstack((array,array_rr,output))
+        op = np.hstack((array[:,:,:-1],array_rr,output))
 
         img = Image.fromarray(op,"RGB")
 
-        img.save(output_path+"/"+name)
+        img.save(output_path+"/"+name.split(".")[0]+"_rr.png")
+        
 
-print(time.time()-start)
+print("FPS:", count/total)
